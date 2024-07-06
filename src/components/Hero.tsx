f390a9eed501c6a73c97590b1e9c9846df817dea
John <mjohncorbet@gmail.com>
2024-07-06 19:46:40 +0800

    fetch data for hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,38 @@
+import { useEffect, useState } from "react";
+import getRequestWithNativeFetch from "../utils/nativeFetch";
+
 export default function Hero() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const thrillerSuspenseUrl = 'https://openlibrary.org/subjects/fiction_thrillers_suspense.json';
+        const historicalFictionUrl = 'https://openlibrary.org/subjects/historical_fiction.json';
+        const selfHelpUrl = 'https://openlibrary.org/subjects/self-help.json';
+
+        async function fetchHeroBooksData() {
+            try {
+                const result = await Promise.all([
+                    getRequestWithNativeFetch(thrillerSuspenseUrl),
+                    getRequestWithNativeFetch(historicalFictionUrl),
+                    getRequestWithNativeFetch(selfHelpUrl)
+                ]);
+
+                const heroData = await result;
+                setData(heroData);
+                setError(null);
+            } catch (err) {
+                if (err instanceof Error) setError(err.message);
+                setData(null);
+            } finally {
+                setLoading(false);
+            }
+        } 
+
+        fetchHeroBooksData();
+    }, []);
+
     return (
         <section className="hero-section">
             <div id="subject-search">
