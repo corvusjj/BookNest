b804ae3f0352d7a45c93a99ca9de60f2c20a0ba6
John <mjohncorbet@gmail.com>
2024-07-16 19:47:25 +0800

    add data-generator for hero section from acquired json, add book cover image

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,16 +1,30 @@
 import { useEffect, useState, useRef } from "react";
 import getRequestWithNativeFetch from "../../utils/nativeFetch";
 
-import BooksBySubject from '../../types/booksBySubject';
 import styles from './Hero.module.scss'
 
 export default function Hero() {
-    const [data, setData] = useState<BooksBySubject[] | null>(null);
+    const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
 
     const carouselRef = useRef<HTMLDivElement | null>(null);
     const carouselIndex = useRef(0);
+
+    function generateHeroData(data) {
+        const randomIndex = Math.floor(Math.random() * 10);
+        const selectedBooks = data.map(subject => subject.works[randomIndex]);
+
+        const heroBookData = selectedBooks.map(book => {
+            const newData = {
+                'title': book.title,
+                'coverURL': `https://covers.openlibrary.org/b/id/${book.cover_id}-M.jpg`
+            };
+            return newData;   
+        });
+
+        return heroBookData;
+    }
 
     useEffect(() => {
         const thrillerSuspenseUrl = 'https://openlibrary.org/subjects/fiction_thrillers_suspense.json';
@@ -25,9 +39,9 @@ export default function Hero() {
                     getRequestWithNativeFetch(selfHelpUrl)
                 ]);
 
-                const heroData= await result;
-                console.log(heroData);
-                setData(heroData);
+                const heroBooksDataJson = await result;
+                const newHeroData =  generateHeroData(heroBooksDataJson);
+                setData(newHeroData);
                 setError(null);
             } catch (err) {
                 if (err instanceof Error) setError(err.message);
@@ -83,19 +97,25 @@ export default function Hero() {
             </button>
             <div className={styles.carouselContainer} ref={carouselRef}>
                 <div className={styles.categoryHighlight} data-highlight="1">
-                    <div className={styles.bookImage}></div>
+                    <div className={styles.bookImage}>
+                        <img src={data && data[0].coverURL} alt="" />
+                    </div>
                     <h2>THRILLERS, SUSPENSE</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet commodi voluptas tempora aut sequi reiciendis?</p>
                     <button>SHOP NOW</button>
                 </div>
                 <div className={styles.categoryHighlight} data-highlight="2">
-                    <div className={styles.bookImage}></div>
+                    <div className={styles.bookImage}>
+                        <img src={data && data[1].coverURL} alt="" />
+                    </div>
                     <h2>HISTORICAL FICTION</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet commodi voluptas tempora aut sequi reiciendis?</p>
                     <button>SHOP NOW</button>
                 </div>
                 <div className={styles.categoryHighlight} data-highlight="3">
-                    <div className={styles.bookImage}></div>
+                    <div className={styles.bookImage}>
+                        <img src={data && data[2].coverURL} alt="" />
+                    </div>
                     <h2>SELF-HELP</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet commodi voluptas tempora aut sequi reiciendis?</p>
                     <button>SHOP NOW</button>
--- src/utils/nativeFetch.tsx
+++
@@ -1,9 +0,0 @@
-export default async function getRequestWithNativeFetch(url:string) {
-    const response = await fetch(url, { mode: 'cors' });
-
-    if (!response.ok) {
-        throw new Error(`HTTP error: Status ${response.status}`);
-    }
-
-    return response.json();
-}
